736fc034708f5c3d5b54c167c588e4662d4d6789
Nicholas Gan Shyan <nicholasganshyan@gmail.com>
2022-06-11 23:59:25 +0800

    allow user to change insample prediction time period

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import streamlit as st
 import pandas as pd
 import calendar
 
+from dataclasses import dataclass
+from typing import Union
+
 import gcs
 import data
 import plots
 import model
 import constant
+
+
+@dataclass
+class Sidebar_Input:
+    timeseries: str
+    insample_start: Union[str, None]
+    insample_end: Union[str, None]
 
 
 st.set_page_config(layout="wide")
@@ -24,18 +34,33 @@ with st.spinner("Downloading data..."):
 st.success("Data downloaded!")
 
 
-def sidebar(ts_names: list[str]):
+def sidebar(ts_names: list[str],
+    insample_start: int, insample_end: int) -> Sidebar_Input:
     """
     Sidebar to allow users to select:
         1. Which variables to predict 
     """
     st.sidebar.write("User Output Selection")
+
+    # In-sample predictions
     insample_pred_name = st.sidebar.selectbox(
-            label="In-sample prediction",
+            label="Insample prediction",
             options=ts_names,
             index=0)
+    insample_start_sel = str(st.sidebar.slider(label="Start",
+        min_value=insample_start, max_value=insample_end, step=1, value=2000))
+    insample_end_sel = str(st.sidebar.slider(label="End",
+        min_value=insample_start, max_value=insample_end, step=1, value=2019))
 
-    return insample_pred_name
+    if insample_end_sel < insample_start_sel:
+        st.sidebar.error("""
+            Insample prediction start year cannot be before end year
+        """)
+        insample_start_sel = insample_end_sel = None
+
+    return Sidebar_Input(timeseries=insample_pred_name,
+        insample_start=insample_start_sel, insample_end=insample_end_sel)
+
 
 def untransformed_data_expander(raw_data, appendix):
     with st.expander("Untransformed data"):
@@ -247,16 +272,14 @@ def principal_components_section(transformed_df, transform_mapping):
             of the total variance.""")
 
 
-def dfm_prediction_section(transformed_df, transform_mapping, insample_ts_name):
+def insample_predictions_selection(training_df, transform_mapping,
+    selection: Sidebar_Input):
     st.header("In-sample predictions")
-    st.subheader(insample_ts_name)
-
-    training_df = model.get_training_dataset(transformed_df)
+    st.subheader(selection.timeseries)
 
     # All in-sample predictions from GCS
-    dfm1_insample_preds, dfm2_insample_preds, dfm3_insample_preds = (
+    insample_preds = (
         gcs.unpack_csv(f"dfm{i}_insample_preds.csv") for i in range(1,4))
-
 
     # Pseudo OOS predictions
     # dfm1_oos_preds = model.nowcasting(model=dfm1_model,
@@ -265,31 +288,27 @@ def dfm_prediction_section(transformed_df, transform_mapping, insample_ts_name):
     #     training_df=training_df, transformed_data_df=transformed_df)
 
     insample_ts_fred = transform_mapping[
-        transform_mapping["fred description"] == insample_ts_name
+        transform_mapping["fred description"] == selection.timeseries
         ]["fred"].iloc[0]
 
-    plots.plot_predictions({
-        "Global Factors Only" :
-        (training_df[:][insample_ts_fred], 
-            dfm1_insample_preds[insample_ts_fred])
-        # "Global & Group Factors":
-        #     (training_df["2000":"2019"][insample_ts_fred], 
-        #     dfm2_insample_preds[insample_ts_fred])
-        # "Group Factors Only":
-        #     (training_df["2000":"2019"][insample_ts_fred], 
-        #     dfm3_insample_preds[insample_ts_fred])
-        }
-    )
+    if ((selection.insample_start is not None) and
+        (selection.insample_end is not None)):
+        insample_start = selection.insample_start
+        insample_end = selection.insample_end
+    else:
+        insample_start = training_df.index[0].year
+        insample_end = training_df.index[-1].year
 
-    st.subheader("Out-of-sample Predictions")
-    # plots.plot_predictions({"Global Factors Only" :
-    #     (transformed_df["2020":"2020"][insample_ts_fred], 
-    #         dfm1_oos_preds[insample_ts_fred]),
-    #     "Global & Group Factors":
-    #         (transformed_df["2000":"2020"][insample_ts_fred], 
-    #         dfm2_oos_preds[insample_ts_fred])
-    #     }
-    # )
+    training_df_sel = training_df[insample_start:insample_end][insample_ts_fred]
+    df1_preds, df2_preds, df3_preds = (
+        pred[insample_start:insample_end][insample_ts_fred]
+        for pred in insample_preds)
+
+    plots.plot_predictions({
+        "Global Factors Only" : (training_df_sel, df1_preds),
+        "Global & Group Factors" : (training_df_sel, df2_preds),
+        "Group Factors Only" : (training_df_sel, df3_preds)
+    })
 
 
 if __name__ == "__main__":
@@ -310,8 +329,13 @@ if __name__ == "__main__":
     data_cleaning_section(raw_data, transformed_df, transform_mapping)
     principal_components_section(transformed_df, transform_mapping)
 
-    insample_pred_name = sidebar(transform_mapping["fred description"])
+    training_df = model.get_training_dataset(transformed_df)
+
+    sidebar_inputs = sidebar(
+        transform_mapping["fred description"],
+        insample_start=training_df.index[0].year,
+        insample_end=training_df.index[-1].year)
 
     # variable_grouping_section(transformed_df, transform_mapping)
-    dfm_prediction_section(transformed_df, transform_mapping,
-        insample_pred_name)
+    insample_predictions_selection(training_df, transform_mapping,
+        sidebar_inputs)
